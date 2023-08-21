02b93404232988674d4a80f5eb3a2d0a304a922e
Xanonymous <trusaidlin@gmail.com>
2023-08-22 01:21:50 +0800

    feat: 🎸 filter online users only for showing

--- src/show_online_status.rs
+++ src/show_online_status.rs
@@ -1,12 +1,27 @@
 use crate::participate_records::*;
+use std::collections::HashMap;
 
 pub(crate) fn show_online_status(raw_log: String) {
     let participate_records = find_participate_records(raw_log);
-    println!("Online players:");
+
+    let mut player_online_status: HashMap<String, i8> = HashMap::new();
+
     for record in participate_records {
-        match record.action {
-            ParticipateActionType::Join => println!("+ {}", record.name),
-            ParticipateActionType::Leave => println!("- {}", record.name),
+        let name = record.name;
+        let action = record.action;
+
+        let status = match action {
+            ParticipateActionType::Join => 1,
+            ParticipateActionType::Leave => -1,
+        };
+
+        let current_status = player_online_status.entry(name).or_insert(0);
+        *current_status += status;
+    }
+
+    for (name, status) in player_online_status {
+        if status > 0 {
+            println!("{} is online", name);
         }
     }
 }
