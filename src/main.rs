1b8752206d99905fe8a8a993ae67fd213ae174b2
Xanonymous <trusaidlin@gmail.com>
2023-08-22 01:36:50 +0800

    feat: 🎸 read stdin for pipe data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::io::{self, BufRead};
 
 use crate::jobs::{Executable, JobKind};
 
@@ -8,16 +9,41 @@ mod participate_records;
 mod show_help_msg;
 mod show_online_status;
 
+fn read_from_stdin() -> String {
+    let stdin = io::stdin();
+
+    // Ensure that no other part of the code can read from stdin simultaneously.
+    let handle = stdin.lock();
+
+    let mut raw_log = String::new();
+
+    for line in handle.lines() {
+        raw_log.push_str(&line.unwrap());
+        raw_log.push('\n');
+    }
+
+    raw_log
+}
+
 fn read_all_args() -> Vec<String> {
     let args: Vec<String> = env::args().collect();
 
     // The first argument is the path of the executable file.
     // The second argument is the name of the job.
     // The third argument is the raw log.
-    if args.len() < 3 {
-        Vec::new()
-    } else {
-        args[1..].to_vec()
+    match args.len() {
+        0 | 1 => Vec::new(),
+        2 => {
+            // get stdin from the user for receiving data from pipe command.
+            let raw_log = read_from_stdin();
+
+            if raw_log.is_empty() {
+                return Vec::new();
+            }
+
+            vec![args[1].clone(), raw_log]
+        }
+        _ => args[1..].to_vec(),
     }
 }
 
